4ecbd90c2c548a724a42feb9b4f15194d7482601
Alex Speranza <alex.speranza@studio.unibo.it>
2024-10-19 17:38:17 +0200

    build: update script to create a table with the workflow status badge

--- .utils/update_readme.sh
+++ .utils/update_readme.sh
@@ -2,7 +2,7 @@
 
 README_PATH="./profile/README.md"
 
-EXCLUDED_REPOSITORIES='["workflows", "test-workflow"]'
+EXCLUDED_REPOSITORIES='["workflows", "test-workflow", ".github"]'
 
 HEADER="$(cat <<HEADER
 # Iosevka NerdFont
@@ -13,13 +13,23 @@ HEADER
 )"
 
 PATCHED_FONT_LIST="$(cat <(echo ""; echo "## Repository list") <(
+    echo "| Variant | Patch status |"
+    echo "| --- | --- |"
+    eval "jq -s -r 'sort_by(.name) | map(\"| [\" + .name + \"](\" + .url + \") | [![Patch font](https://github.com/Iosevka-NerdFont/\" + .name + \"/actions/workflows/patch.yaml/badge.svg?branch=main)](https://github.com/Iosevka-NerdFont/\" + .name +\"/workflows/patch.yaml) |\") | .[]'" <(
     curl -fsSL \
         -H "Accept: application/vnd.github+json" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        https://api.github.com/orgs/Iosevka-NerdFont/repos | \
-        eval "jq -r 'map(select(.name as \$value | ${EXCLUDED_REPOSITORIES} | index(\$value) == null)) | map({\"name\": .name, \"url\": .html_url}) | sort_by(.name) | .[] | \"* [\" + .name + \"](\" + .url + \")\"'"
-)
+        "https://api.github.com/orgs/Iosevka-NerdFont/repos?per_page=100&page=1" | \
+        eval "jq -r 'map(select(.name as \$value | ${EXCLUDED_REPOSITORIES} | index(\$value) == null)) | map({\"name\": .name, \"url\": .html_url}) | .[]'"
+    curl -fsSL \
+        -H "Accept: application/vnd.github+json" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        "https://api.github.com/orgs/Iosevka-NerdFont/repos?per_page=100&page=2" | \
+        eval "jq -r 'map(select(.name as \$value | ${EXCLUDED_REPOSITORIES} | index(\$value) == null)) | map({\"name\": .name, \"url\": .html_url}) | .[]'"
+    )
+    )
 )"
+
 
 echo "${HEADER}" > "${README_PATH}"
 echo "${PATCHED_FONT_LIST}" >> "${README_PATH}"
